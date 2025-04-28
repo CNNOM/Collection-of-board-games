220cc13e745d29335c5c353a5087a59ab7245f41
Алексей Зенков <zenkov.m700@mail.ru>
2025-04-28 21:26:18 +0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.collection_board_games {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.collection_board_games to javafx.fxml;
+    exports com.example.collection_board_games;
+}
